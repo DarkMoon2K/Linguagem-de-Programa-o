9a88b98bc1fc96589e4a804da2899a747f476a9d
Kaua Seraphim <kauahenrique7@gmail.com>
2024-05-17 19:13:28 -0300

    update readme

--- AC2/exercicios_aula_8/exercicio_1/script.js
+++
@@ -1,9 +0,0 @@
-let botao = document.getElementById('botao')
-let divisao = document.getElementById('div1')
-const cores = ['white','red', 'blue', 'pink', 'yellow', 'white', 'orange', 'gray', 'purple', 'coral']
-function mudarCor() {
-    divisao.style.backgroundColor = cores[Math.floor(Math.random()* (10 + 1 + 1)) + 1]
-}
-
-botao.addEventListener('click', mudarCor)
-
